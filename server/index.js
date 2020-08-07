7c0b1473bfe8f39f09d1e7f48eab46d85aa1524b
GC54 <sahilpohare>
2020-08-07 18:28:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,30 +3,42 @@ const app = express();
 const {PythonShell} = require('python-shell');
 const bodyParser = require('body-parser');
 
-const shell = new PythonShell('new_parser.py',{
-    args : [
-        '-i',
-        'invite.html'
-    ]
-});
+// const shell = new PythonShell('new_parser.py',{
+//     args : [
+//         '-i',
+//         'invite.html'
+//     ]
+// });
 
-shell.on('message',(msg)=>{
-    console.log(JSON.parse(msg));
+// shell.on('message',(msg)=>{
+//     console.log(JSON.parse(msg));
 
+// })
+
+// shell.end(function (err,code,signal) {
+//     if (err) throw err;
+//     console.log('The exit code was: ' + code);
+//     console.log('The exit signal was: ' + signal);
+//     console.log('finished');
+// });
+
+app.use(bodyParser.urlencoded(),bodyParser.json());
+
+app.get('/',(req,res)=>res.send('Mail Parse'))
+
+app.get('/parser',async (req,res)=>{
+    try {
+        await PythonShell.run('./new_parser.py',{
+            args : [
+                '-b',
+                req.body.content
+            ],
+        }, (err, result) =>{
+            res.json(JSON.parse(result));
+        });
+    } catch (e){
+        console.log(e);
+    }
 })
 
-shell.end(function (err,code,signal) {
-    if (err) throw err;
-    console.log('The exit code was: ' + code);
-    console.log('The exit signal was: ' + signal);
-    console.log('finished');
-});
-
-app.use(bodyParser.json());
-
-app.get('/parser',(req,res)=>{
-    res.send('');
-    req.body 
-})
-
-app.listen(3200,console.log('Jingling on 3000'))
+app.listen(3200,console.log('Jingling on 3200'))
