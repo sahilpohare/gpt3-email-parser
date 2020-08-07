5c94c9123749807989495efe53a03ff23d0dee62
rahul81 <rahuldas.rr81@gmail.com>
2020-08-07 15:38:46 +0530

    json output

--- parser/new_parser.py
+++ parser/new_parser.py
@@ -1,6 +1,8 @@
 from converter import convert
 from gpt_train import parse
 import os 
+import pandas as pd 
+import json
 
 PATH = './test_html'
 
@@ -17,12 +19,11 @@ test = read_html(PATH+'/invite2.html')
 print(test)
 
 response = parse(test)
+response = json.loads(response)
 
+print(type(response))
 print(response)
 
-#Now parse this mail the train gpt
-
-# print(gpt_invite(mail_test))
 
 
 
