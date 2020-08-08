642a9edd9e23939f3769115e38e5d8d46ad15950
GC54 <sahilpohare>
2020-08-08 17:31:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,35 +3,16 @@ const app = express();
 const {PythonShell} = require('python-shell');
 const bodyParser = require('body-parser');
 
-// const shell = new PythonShell('new_parser.py',{
-//     args : [
-//         '-i',
-//         'invite.html'
-//     ]
-// });
-
-// shell.on('message',(msg)=>{
-//     console.log(JSON.parse(msg));
-
-// })
-
-// shell.end(function (err,code,signal) {
-//     if (err) throw err;
-//     console.log('The exit code was: ' + code);
-//     console.log('The exit signal was: ' + signal);
-//     console.log('finished');
-// });
-
 app.use(bodyParser.urlencoded(),bodyParser.json());
 
 app.get('/',(req,res)=>res.send('Mail Parse'))
 
-app.get('/parser',async (req,res)=>{
+app.get('/parse',async (req,res)=>{
     try {
         await PythonShell.run('./new_parser.py',{
             args : [
                 '-b',
-                req.body.content
+                JSON.stringify(req.body)
             ],
         }, (err, result) =>{
             res.json(JSON.parse(result));
