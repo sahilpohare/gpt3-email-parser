62c77aaee9fadcc3984a8d25167026cd6007f20e
nikhilm21 <57938335+nikhilm21@users.noreply.github.com>
2020-08-10 14:02:11 +0530

    working server

--- server/new_parser.py
+++ server/new_parser.py
@@ -4,21 +4,19 @@ import os
 import json
 import argparse
 
-'''
+
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('-i','--input', type=str, default=None)
-# argparser.add_argument('-r','--run', type=str, default=False)
 argparser.add_argument('-b','--body', type=str, default=None)
 
 args = argparser.parse_args()
 
 filename = args.input
-# run_all = args.run
 body = args.body
+body = json.loads(body)['content']
 
-'''
-print('YES')
+
 
 
 
@@ -35,36 +33,19 @@ def read_html(path):
 
 def run(filename):
     try:
-        
         test = read_html(os.path.join(PATH,filename))
-        #print(test)
         response = parse(test)
         print(response)
-        #print(' ')
-        # with open('./results/'+filename.split('.')[0]+'.json','w') as f:
-        #     json.dump(response, f)
-        #     f.close()
         return response
     except Exception as e:
         print(e)
 
 
 
-# if filename != None:
-#     run(filename)
-# elif body != None:
-#     response = parse(convert(body))
-#     print(response)
-
-
-mails = ('add_email.html','invite.html')
-
-test = []
-for mail in mails:
-    print(maill)
-
-    test.append(read_html(os.path.join(PATH,mail)))
-print(test)
-
+if filename != None:
+    run(filename)
+elif body != None:
+    response = parse(convert(body))
+    print(response)
 
     
