58bdb449974ce8ca22c479a16bb82f9b7adbbd67
Rahul Das <rahuldas.rr81@gmail.com>
2020-08-10 13:04:10 +0530

    Update gpt_train.py

--- server/gpt_train.py
+++ server/gpt_train.py
@@ -21,7 +21,10 @@ train_mails = {
     'Auth.html':'resp1',
     'email_added.html':'resp4',
     'invite1.html':'resp2',
-    'verification.html':'resp3'
+    'verification.html':'resp3',
+    'verify_email.html':'resp5',
+    'pass_reset_conf.html':'resp6',
+    'inviteTrello.html':'resp7',
 }
 
 
@@ -29,9 +32,11 @@ resp = {
     'resp1':'{"Type":"Auth", "name":"rahul81", "application":"Dockship", "link":"https://github.com/settings/connections/applications/ccb42a8e19665bcd148e", "From":"The GitHub Team"}',
     'resp2':'{"Type":"Invitation", "invitor":"rahul81", "repo":"GPT3parser", "repolink":"https://github.com/rahul81/GPT3parser", "For":"sahilpohare@gmail.com"}',
     'resp3':'{"Type":"Verfication", "name":"rahul81", "Device":"Linux", "verification_code":"849680", "From":"The GitHub Team"}',
-    'resp4':'{"Type":"email_added", "name":"rahul81", "new_email":"rahul.das@adypu.edu.in", "From":"The GitHub Team"}'
+    'resp4':'{"Type":"email_added", "name":"rahul81", "new_email":"rahul.das@adypu.edu.in", "From":"The GitHub Team"}',
+    'resp5':'{"Type":"email_verify", "name":"rahul81", "email":"rahuldas.rr81@gmail.com", "From":"GitHub" }',
+    'resp6':'{"Type":"password_reset_confirmation", "name":"rahul81", "email":"rahuldas.rr81@gmail.com"}',
+    'resp7':'{"Type":"Invite", "inviter":"Rahul D", "board":"Turista-SIH2020","From":"Trello"}',
 }
-
 
 
 
