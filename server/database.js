0b833f678b93cdd04e3fbb6c3b857721f7f1ea7c
GC54 <sahilpohare>
2020-08-11 13:40:10 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,5 +1,15 @@
 const app = require('express')();
 const { JsonDB } = require('node-json-db');
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const db = new JsonDB(new Config('./db/mailTrain', true, true, '/'));
+
+var extractDomain = (addr) => addr.split('@')[1].split('.')[0];
+/*
+    {
+        prompt : {
 
         },
         answer : {
