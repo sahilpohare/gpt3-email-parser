f3a572c45f9fbaa3ae7096fcf193dbf02ea0b738
rahul81 <rahuldas.rr81@gmail.com>
2020-08-13 13:50:06 +0530

    train data merged

--- server/new_parser.py
+++ server/new_parser.py
@@ -1,5 +1,5 @@
 from converter import convert
-# from gpt_train import parse
+from gpt_train import parse
 import os 
 import json
 import argparse
@@ -26,14 +26,12 @@ if body != None:
 PATH = '.server/test_html'
 
 def process(filename):
-    mail = read_html(os.path.join(PATH,filename))
-    mailtest = re.split('\n\n|--',mail)
-    fin1 = []
-    for e,i in enumerate(mailtest):
-        if e%2 != 0:
-            fin1.append(i)
-    fin1 = fin1[:-3]
-    for i in fin1:
+    if filename != None and body == None:
+        mail = read_html(os.path.join(PATH,filename))
+    else:
+        mail = filename
+    mailtest = re.split('boards.trello.com|on Trello|--',mail)[1:-2]
+    for i in mailtest:
         print(i)
         run(i)
 
@@ -43,7 +41,7 @@ def read_html(path):
     with open(path,'r') as f:
         resp = f.read()
         f.close()
-        if domain not in domainsToProcess:
+        if domain in domainsToProcess:
             return resp
 
         resp = convert(resp)
@@ -58,6 +56,7 @@ def run(filename):
         else:
             response = parse(filename)
         response = response[:response.find('}')+1]
+        response = json.dumps(response.replace("'",'"'))
         print(response)
         return response
     except Exception as e:
@@ -69,10 +68,8 @@ if domain in domainsToProcess:
     if filename != None:
         process(filename)
     elif body != None:
-        response =parse(body)
-        response = response[:response.find('}')+1]
-        response = json.dumps(response.replace("'",'"'))
-        print(response)
+        process(body)
+
 else:
     if filename != None:
         run(filename)
